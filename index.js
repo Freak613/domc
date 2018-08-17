c61160b3c3cb1722486d836d45866be3d86bb05c
Pavel Martynov <martynov.pl@gmail.com>
2018-08-17 14:55:41 +0300

    Custom directives

--- index.js
+++ index.js
@@ -34,163 +34,216 @@ function setupSyntheticEvent(name) {
 // - str[idx] and str.slice are faster than regex matching
 //
 
-let attr, aname, avalue, eventType, 
-reactiveValue,
-vdomCode, compareCode, args, 
-vdomId, arg, refsCode, 
-nodeData, eventHandler, parenIdx,
-eventHandlerArgs, varCode, nodeType
-
-function codegen(node, pathId) {
-    nodeType = node.nodeType
-    
-    if (nodeType !== 3) {
-
-        // codegenAttributes
-        if (node.attributes !== undefined) {
-            for(attr of node.attributes) {
-                aname = attr.name
-                avalue = attr.value
-
-                if (aname[0] === 'o' && aname[1] === 'n') {
-
-                    eventType = aname.slice(2)
-                    setupSyntheticEvent(eventType)
-
-                    if (avalue.indexOf("${") >= 0) {
-                        reactiveValue = avalue.slice(2, avalue.length - 1)
-                        parenIdx = reactiveValue.indexOf("(")
-                        eventHandler = reactiveValue.slice(0, parenIdx)
-                        eventHandlerArgs = reactiveValue.slice(parenIdx + 1, reactiveValue.length - 1).split(',')
-
-                        vdomId = makeid.possible.charAt(makeid.counter++)
-
-                        refsCode += `const ${vdomId} = node.__${vdomId} = ${pathId};\n`
-                        refsCode += `${vdomId}.__${eventType} = scope.${eventHandler};\n`
-                        vdomCode += `vdom.${vdomId} = ${eventHandlerArgs};\n`
-                        compareCode +=`if (current.${vdomId} !== vdom.${vdomId}) node.__${vdomId}.__${eventType}Data = vdom.${vdomId};\n`
-
-                        node.removeAttribute(aname)
-                    }
-
-                } else if (avalue.indexOf("${") >= 0) {
-                    if (aname === 'class') {
-                        vdomId = makeid.possible.charAt(makeid.counter++)
-
-                        refsCode += `node.__${vdomId} = ${pathId};\n`
-                        vdomCode += `vdom.${vdomId} = ${avalue.slice(2, avalue.length - 1)};\n`
-                        compareCode +=`if (current.${vdomId} !== vdom.${vdomId}) node.__${vdomId}.className = vdom.${vdomId};\n`
-                    } else {
-                        vdomId = makeid.possible.charAt(makeid.counter++)
-
-                        refsCode += `node.__${vdomId} = ${pathId};\n`
-                        vdomCode += `vdom.${vdomId} = ${avalue.slice(2, avalue.length - 1)};\n`
-                        compareCode +=`if (current.${vdomId} !== vdom.${vdomId}) node.__${vdomId}.setAttribute("${aname}", vdom.${vdomId});\n`
-                    }
-
-                    node.removeAttribute(aname)
-                }   
-            }
-        }
-        // End codegenAttributes
-
-    } else {
-
-        // codegenText
-        nodeData = node.nodeValue
-        if (nodeData.indexOf("${") >= 0) {
-            vdomId = makeid.possible.charAt(makeid.counter++)
-
-            refsCode += `node.__${vdomId} = ${pathId};\n`
-            vdomCode += `vdom.${vdomId} = ${nodeData.slice(2, nodeData.length - 1)};\n`
-            compareCode +=`if (current.${vdomId} !== vdom.${vdomId}) node.__${vdomId}.nodeValue = vdom.${vdomId};\n`
-
-            node.nodeValue = ""
-        }
-        // End codegenText
-
-    }
-}
-
-// Inspired by: https://gist.github.com/cowboy/958000
-function walker(node) {
-    let skip = false, tmp
-    let pathId = 'node', prevPathId
-    codegen(node, pathId)
-    pathId = ''
-    do {
-        if (!skip && (tmp = node.firstChild)) {
-            if (tmp.nodeType === 3 && tmp.nodeValue.trim() === "") {
-                tmp.parentNode.removeChild(tmp)
-                continue
-            }
-
-            prevPathId = pathId
-            pathId += '_f'
-            varCode += `let ${pathId} = ${prevPathId || 'node'}.firstChild;\n` 
-            
-            codegen(tmp, pathId)
-
-            skip = false
-        } else if (tmp = node.nextSibling) {
-            if (tmp.nodeType === 3 && tmp.nodeValue.trim() === "") {
-                tmp.parentNode.removeChild(tmp)
-                continue
-            }
-
-            prevPathId = pathId
-            pathId += '_n'
-            varCode += `let ${pathId} = ${prevPathId || 'node'}.nextSibling;\n` 
-            
-            codegen(tmp, pathId)
-
-            skip = false
-        } else {
-            pathId = pathId.slice(0, pathId.length - 2)
-            tmp = node.parentNode
-            skip = true
-        }
-        node = tmp
-    } while (node)
-}
-
 function makeid() {}
 makeid.possible = "abcdefghijklmnopqrstuvwxyz"
 makeid.counter = 0
 
-function codeopt() {
-    const vars = varCode.match(/_f\w*/g)
-    let i = vars.length, _var
-    while(--i) {
-        _var = vars[i]
-        if (varCode.indexOf(` ${_var}.`) === -1 && refsCode.indexOf(` ${_var};`) === -1) {
-            varCode = varCode.replace(new RegExp(`let ${_var} = .*?;\n`), '')
+
+export const customDirectives = {}
+
+class Compiler {
+    constructor() {
+        this.varCode = 
+        this.vdomCode = 
+        this.compareCode = 
+        this.refsCode = 
+        this.directiveSetupCode = 
+        this.directiveUpdateCode = ''
+    }
+
+    // Inspired by walker: https://gist.github.com/cowboy/958000
+    compile(root) {
+        let skip = false, tmp, pathId = 'node', prevPathId, pahtIdLen, node = root, canIGoDeep
+        this.codegen(node, pathId)
+        pathId = ''
+        do {
+            if (!skip && (tmp = node.firstChild)) {
+                if (tmp.nodeType === 3 && tmp.nodeValue.trim() === "") {
+                    tmp.parentNode.removeChild(tmp)
+                    continue
+                }
+                skip = false
+
+                prevPathId = pathId
+                pathId += '_f'
+                this.varCode += `let ${pathId} = ${prevPathId || 'node'}.firstChild;\n` 
+                
+                canIGoDeep = this.codegen(tmp, pathId)
+                if (canIGoDeep > 0) skip = true
+            } else if (tmp = node.nextSibling) {
+                if (tmp.nodeType === 3 && tmp.nodeValue.trim() === "") {
+                    tmp.parentNode.removeChild(tmp)
+                    continue
+                }
+                skip = false
+
+                prevPathId = pathId
+                pathId += '_n'
+                this.varCode += `let ${pathId} = ${prevPathId || 'node'}.nextSibling;\n` 
+                
+                canIGoDeep = this.codegen(tmp, pathId)
+                if (canIGoDeep > 0) skip = true
+            } else {
+                pahtIdLen = pathId.length
+                if (pathId[pahtIdLen - 1] === 'n') {
+                    pathId = pathId.slice(0, pathId.lastIndexOf('_f_n'))
+                } else {
+                   pathId = pathId.slice(0, pahtIdLen- 2) 
+                }
+                tmp = node.parentNode
+                skip = true
+            }
+            node = tmp
+            if (node === root) break
+        } while (node)
+
+        // this.codeopt()
+    }
+
+    codegen(node, pathId) {
+        const nodeType = node.nodeType
+        
+        if (nodeType !== 3) {
+
+            // codegenAttributes
+            if (node.attributes !== undefined) {
+                for(let attr of node.attributes) {
+                    const aname = attr.name
+                    const avalue = attr.value
+
+                    if (aname[0] === 'v' && aname[1] === '-') {
+                        node.removeAttribute(aname)
+
+                        const directive = aname.slice(2)
+
+                        const vdomId = makeid.possible.charAt(makeid.counter++)
+
+                        this.directiveSetupCode += `node.__${vdomId} = CD.${directive}(${pathId}, scope, "${avalue}");\n`
+                        this.directiveUpdateCode += `node.__${vdomId}(scope);\n`
+
+                        return 1
+                    }
+
+                    if (aname[0] === 'o' && aname[1] === 'n') {
+
+                        const eventType = aname.slice(2)
+                        setupSyntheticEvent(eventType)
+
+                        if (avalue.indexOf("${") >= 0) {
+                            const reactiveValue = avalue.slice(2, avalue.length - 1)
+                            const parenIdx = reactiveValue.indexOf("(")
+                            const eventHandler = reactiveValue.slice(0, parenIdx)
+                            const eventHandlerArgs = reactiveValue.slice(parenIdx + 1, reactiveValue.length - 1).split(',')
+
+                            const vdomId = makeid.possible.charAt(makeid.counter++)
+
+                            this.refsCode += `const ${vdomId} = node.__${vdomId} = ${pathId};\n`
+                            this.refsCode += `${vdomId}.__${eventType} = scope.${eventHandler};\n`
+
+                            if (parenIdx < reactiveValue.length - 2) {
+                                this.vdomCode += `vdom.${vdomId} = ${eventHandlerArgs};\n`    
+                                this.compareCode +=`if (current.${vdomId} !== vdom.${vdomId}) node.__${vdomId}.__${eventType}Data = vdom.${vdomId};\n`
+                            }
+
+                            node.removeAttribute(aname)
+                        }
+
+                    } else if (avalue.indexOf("${") >= 0) {
+                        if (aname === 'class') {
+                            const vdomId = makeid.possible.charAt(makeid.counter++)
+
+                            this.refsCode += `node.__${vdomId} = ${pathId};\n`
+                            this.vdomCode += `vdom.${vdomId} = ${avalue.slice(2, avalue.length - 1)};\n`
+                            this.compareCode +=`if (current.${vdomId} !== vdom.${vdomId}) node.__${vdomId}.className = vdom.${vdomId};\n`
+                        } else {
+                            const vdomId = makeid.possible.charAt(makeid.counter++)
+
+                            this.refsCode += `node.__${vdomId} = ${pathId};\n`
+                            this.vdomCode += `vdom.${vdomId} = ${avalue.slice(2, avalue.length - 1)};\n`
+                            this.compareCode +=`if (current.${vdomId} !== vdom.${vdomId}) node.__${vdomId}.setAttribute("${aname}", vdom.${vdomId});\n`
+                        }
+
+                        node.removeAttribute(aname)
+                    }   
+                }
+            }
+            // End codegenAttributes
+
+        } else {
+
+            // codegenText
+            const nodeData = node.nodeValue
+            if (nodeData.indexOf("${") >= 0) {
+                const vdomId = makeid.possible.charAt(makeid.counter++)
+
+                this.refsCode += `node.__${vdomId} = ${pathId};\n`
+                this.vdomCode += `vdom.${vdomId} = ${nodeData.slice(2, nodeData.length - 1)};\n`
+                this.compareCode +=`if (current.${vdomId} !== vdom.${vdomId}) node.__${vdomId}.nodeValue = vdom.${vdomId};\n`
+
+                node.nodeValue = ""
+            }
+            // End codegenText
+
         }
+
+        return 0
+    }
+
+    codeopt() {
+        let varCode = this.varCode,
+            refsCode = this.refsCode
+
+        if (varCode.length === 0) return
+
+        const vars = varCode.match(/_f\w*/g)
+        let i = vars.length, _var
+        while(--i) {
+            _var = vars[i]
+            if (varCode.indexOf(` ${_var}.`) === -1 && refsCode.indexOf(` ${_var};`) === -1) {
+                varCode = varCode.replace(new RegExp(`let ${_var} = .*?;\n`), '')
+            }
+        }
+
+        this.varCode = varCode
+    }
+
+    createFn() {
+        return Function("node", "scope", "CD", this.varCode + '\n' + this.refsCode + '\n' + this.directiveSetupCode)
+    }
+    updateFn(scope) {
+        let argsStr = ''
+        for(let arg of Object.keys(scope)) argsStr += arg + ","
+        return Function("scope", 'const {' + argsStr + '} = scope;\nconst node = this;\nconst current = node.__vdom || {};\nconst vdom = {};\n' + this.vdomCode + this.compareCode + "node.__vdom = vdom;" + '\n\n' + this.directiveUpdateCode)
     }
 }
  
-let templateInstance
 class Template {
-    constructor(dom) {
-        varCode = vdomCode = compareCode = refsCode = ''
+    constructor(dom, createFn, updateFn) {
         this.dom = dom
-        walker(dom)
-        codeopt()
-        this.create = Function("dom", "scope", `let node = dom.cloneNode(true);\n\n` + varCode + '\n' + refsCode + `\nreturn node;`)
-        this.update = Function("{" + args + "}", 'const node = this;\nconst current = node.__vdom || {};\nconst vdom = {};\n' + vdomCode + compareCode + "node.__vdom = vdom;")
+        this.create = createFn
+        this.update = updateFn
     }
     createInstance(scope) {
-        templateInstance = this.create(this.dom, scope)
-        templateInstance.update = this.update
-        templateInstance.update(scope)
-        return templateInstance
+        const node = this.dom.cloneNode(true)
+        this.create(node, scope, customDirectives)
+        node.update = this.update
+        node.update(scope)
+        return node
+    }
+    rehydrate(scope) {
+        this.create(this.dom, scope, customDirectives)
+        this.dom.update = this.update
+        this.dom.update(scope)
     }
 }
 
 function domc(dom, scope) {
-    args = ''
-    for(arg of Object.keys(scope)) args += arg + ","
-    return new Template(dom)
+    const c = new Compiler()
+    c.compile(dom)
+    const createFn = c.createFn()
+    const updateFn = c.updateFn(scope)
+    // console.debug({createFn, updateFn})
+    return new Template(dom, createFn, updateFn)
 }
 
 export default domc
